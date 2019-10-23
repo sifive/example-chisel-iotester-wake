6e1b986bef5bf4ffc4872cb4a2788d9eb5a6c797
Liwei Ma <liwei.ma@sifive.com>
2019-10-23 19:55:50 +0800

    add iotesters2

---
+++ src/test/scala/SyntaxIotesters2.scala
@@ -0,0 +1,28 @@
+package syntax
+
+import chisel3._
+import chisel3.experimental._
+import chisel3.tester._
+import org.scalatest.FreeSpec
+import chisel3.experimental.BundleLiterals._
+
+class NullTester2 extends FreeSpec with ChiselScalatestTester {
+  println ("hello world")
+
+  println(System.getProperty("java.version"))
+  println(System.getProperty("java.runtime.version"))
+
+  "Basic test in chisel3.iotesters2" in {
+    test(new Null) { dut =>
+      dut.io.a.poke(2.U)
+      dut.io.b.poke(1.U)
+      dut.clock.step()
+      dut.io.c.expect(3.U)
+
+      dut.io.a.poke(2.U)
+      dut.io.b.poke(2.U)
+      dut.clock.step()
+      dut.io.c.expect(0.U)
+    }
+  }
+}
